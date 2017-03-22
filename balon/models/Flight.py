d515def0cf81fa38861e9aad2b4f636d0c5e3655
Jakub Findura <findurajakub@gmail.com>
2017-03-22 14:16:52 +0100

    [FIX] Flight constructor fix

--- balon/models/Flight.py
+++ balon/models/Flight.py
@@ -10,15 +10,17 @@ class Flight(object):
     def __init__(self,*args,**kwargs):
         if kwargs.has_key("fromDB"):
             fromDB = kwargs["fromDB"]
+            if fromDB is None: raise ValueError()
+
             self.id = fromDB["id"]
             self.number = fromDB["number"]
             self.hash = fromDB["hash"]
             self.start_date = fromDB["start_date"]
         else:
             self.id = None
-            self.number = None
-            self.hash = None
-            self.start_date = None
+            self.number = kwargs["number"]
+            self.hash = kwargs["hash"]
+            self.start_date = kwargs["start_date"]
 
     def __str__(self):
         return '<Flight [%d] [%s] %d / %s  >' % (self.id, str(self.start_date), self.number, self.hash)
